5b6cb6f5dc04910b18d0d121b3ea190389d2ebf0
br0kenpixel <23280129+br0kenpixel@users.noreply.github.com>
2025-04-04 09:39:11 +0200

    add CPU overclock

--- main.py
+++ main.py
@@ -2,14 +2,17 @@ from sysc.logging import os_info, os_debug, os_error
 from sysc.battery import Battery
 from sysc.boardled import BoardLed
 from firmware import fw_main
-from machine import I2C, Pin, deepsleep
+from machine import I2C, Pin, deepsleep, freq
 from sysc.config import sys as sysconfig
 from sysc.config.app import AppConfig
-from time import time
+from time import time_ns
 from sys import print_exception
 
 os_info("pwos", "PixelWeatherOS Lite")
 os_info("pwos", "(C) Fábián Varga 2025")
+
+# Overclock to 240MHz
+freq(240_000_000)
 
 os_debug("pwos", "Initializing system LED")
 led = BoardLed(sysconfig.ONBOARD_LED_PIN, sysconfig.ONBOARD_LED_INVERT)
@@ -24,7 +27,7 @@ i2c = I2C(1, scl = 8, sda = 5, freq = 100000)
 os_debug("pwos", "Initializing app configuration")
 appcfg = AppConfig()
 
-start = time()
+start = time_ns()
 
 os_info("pwos", "Starting firmware")
 try:
@@ -34,8 +37,8 @@ except Exception as ex:
     os_error("pwos", "Firmware failed: " + repr(ex))
     print_exception(ex)
 
-elapsed = time() - start
-os_info("pwos", "Tasks completed in " + str(elapsed) + "s")
+elapsed = ((time_ns() - start) / 1000000) / 1000 # convert ns to secs
+os_info("pwos", "Tasks completed in %.02fs" % elapsed)
 
 #os_debug("pwos", "Sleeping for " + str(appcfg.sleep_time) + "s")
 #deepsleep(appcfg.sleep_time * 1000)
